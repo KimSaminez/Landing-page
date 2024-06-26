135720ec269919f999ac23375470c862839fdfa4
Miguel Willame <124123044+willameluis@users.noreply.github.com>
2024-06-25 21:33:37 -0400

    Menu Mobile funcional

---
+++ javascript/home.js
@@ -0,0 +1,8 @@
+function showButton() {
+    let menuMobile = document.querySelector('.menu_mobile');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open')
+    } else {
+        menuMobile.classList.add('open');
+    }
+}
